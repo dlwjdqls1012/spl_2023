d8c681de2e21f77add90a7622c482f2df3d830d5
dlwjdqls1012 <64800435+dlwjdqls1012@users.noreply.github.com>
2023-09-01 11:54:38 +0900

    Add files via upload

--- 0901.sql
+++ 0901.sql
@@ -47,40 +47,40 @@ having count(*) >=2
 order by custid;
 
 --1번문제
-
+--도서번호가 1인 도서의 이름
 select * from book where bookid = 1;
-
+--가격이 20000원 이상인 도서의 이름
 select * from book where price >= 20000;
-
+--박지성의 총 구매액
 select sum(saleprice)
 from orders
 where custid = (select custid
 from customer
 where name = '박지성');
-
+--박지성이 구매한 도서의 수
 select count(*)
 from orders
 where custid = 1;
 
 --2번문제
-
+--마당서점 도서의 총 개수
 select count(*)
 from book;
-
+--마당서점에 도서를 출고하는 출판사의 총 개수
 select count(DISTINCT publisher)
 from book;
-
+--모든 고객의 이름,주소
 select  name, address
 from customer;
-
+--2023년 11월 12일 부터 2023년 11월 30일 사이에 주문 받은 도서의 주문번호
 select orderid from orders where orderdate between '23-11-12' and '23-11-30';
-
+--2023년 11월 12일 부터 2023년 11월 30일 제외한 주문 받은 도서의 주문번호
 select orderid from orders where orderdate not between '23-11-12' and '23-11-30';
-
+--성이 권씨인 고색의 이름과 주소
 select name, address
 from customer
 where name like '권%';
-
+--성이 권씨이고 이름이 헌으로 끝나는 고객의 이름과 주소
 select name, address
 from customer
 where name like '권_헌';
